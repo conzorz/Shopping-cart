b388d0f705e6c8c329f1bd9d90a21d0dc01ece27
BatteryAid_AndroidApp <batteryaid@gmail.com>
2018-04-07 16:57:24 +0100

    Step 3

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,20 @@
 
 public class Test {
-	private int
-	apple = 60, orange = 25;
-	private int[] list;
-	
-	public void add(int[] Array) {
+	public static void main() {
+		int apple = 60, orange = 25;
+		int[] list= {};
+		
 		int total=0;
 		for (int i=1; i<list.length; i++) {
-			total+=Array[i];
+			total+=list[i];
 			System.out.println("Price: " + total);
 		}
+		
+		
+		/*void test() {
+			int[] list2 = {apple, apple, orange, apple};
+			add(list2);
+		}
+		*/
 	}
 }
