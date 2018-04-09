a1541a154380430039382d14822189f9679712ed
BatteryAid_AndroidApp <batteryaid@gmail.com>
2018-04-09 15:25:33 +0100

    Step 2.1

--- src/Test.java
+++ src/Test.java
@@ -2,24 +2,34 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Test {
+	static int apple = 60, orange = 25;
+	static int[] list= {apple, apple, orange, apple};
+	static int total=0;
+	static NumberFormat n = NumberFormat.getCurrencyInstance(Locale.UK);
+	
 	public static void main(String[] args) {
-		int apple = 60, orange = 25;
-		int[] list= {apple, apple, orange, apple};
+		//Step 2.1
+		boolean freeApple = false;
 		
-		int total=0;
-		NumberFormat n = NumberFormat.getCurrencyInstance(Locale.UK);
 		for (int i=0; i<list.length; i++) {
+			//Step 2.1
+			if (list[i] == apple) {
+				checkout(i, freeApple);
+				freeApple = !freeApple;
+			}
+			else
+				//Orange
+				checkout(i, false);
+		}
+	}
+	
+	public static void checkout(int i, boolean freeApple) {
+		if (freeApple)
+			System.out.println("Free apple");
+		else {
 			total+=list[i];
-			long formatPrice = total;
 			String price = n.format(total / 100.0);
 			System.out.println("Price: " + price);
 		}
-		
-		
-		/*void test() {
-			int[] list2 = {apple, apple, orange, apple};
-			add(list2);
-		}
-		*/
 	}
 }
